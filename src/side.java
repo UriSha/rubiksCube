1a91445e9a18a019343edd3ec47480407001cad5
uri <urish92@gmail.com>
2017-11-02 23:41:38 +0200

    URI & OMER

--- src/side.java
+++ src/side.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class side{
     public enum Direction{
@@ -26,6 +27,7 @@ public class side{
     }
 
     public static void main(String[] args) {
+
         System.out.println("hello world");
     }
 }
