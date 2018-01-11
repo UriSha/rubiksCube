05e5e7f588f1297ae62fb07254d5bf4e8229fbc0
Ori Haviv <orihaviv@gmail.com>
2018-01-11 18:10:00 +0200

    logic test without str.format

--- src/LogicTest.java
+++ src/LogicTest.java
@@ -30,13 +30,16 @@ public class LogicTest {
             Cube cube = CubeUtils.getRandomCube();
             List<AlgorithmCommands> actions = new ArrayList<>();
             Logic.initialize(cube, actions);
-            String msg = "Initialize test has failed. The center of the %s face is: %s instead of: %s";
-            assertTrue(String.format(msg,"UP", cube.getUp().getGrid()[1][1],"RED"),
+            String msg1 = "Initialize test has failed. The center of the";
+            String msg2 = "face is: ";
+            String msg3 = "instead of: ";
+            assertTrue(msg1 + "UP" + msg2 + cube.getUp().getGrid()[1][1] + msg3 + "RED",
                     cube.getUp().getGrid()[1][1] == Cube.Color.RED);
-            assertTrue( String.format(msg,"FRONT", cube.getFront().getGrid()[1][1],"YELLOW"),
+            assertTrue(msg1 + "FRONT" + msg2 + cube.getFront().getGrid()[1][1] + msg3 + "YELLOW",
                     cube.getFront().getGrid()[1][1] == Cube.Color.YELLOW);
         }
     }
+
     @Test
     public void stageOne() {
         for (int i = 0; i < 100; i++) {
@@ -44,7 +47,9 @@ public class LogicTest {
             List<AlgorithmCommands> actions = new ArrayList<>();
             Logic.initialize(cube, actions);
             Logic.stageOne(cube, actions);
-            String msgCross = "Stage one test has failed. %s edge is located at : %s - %s  instead of : %s";
+            String msg1 = "Stage one test has failed. ";
+            String msg2 = " edge is located at : ";
+            String msg3 = "  instead of : ";
             Logic.Location redYellow = LogicUtils.getLocationOfEdge(cube, Cube.Color.RED, Cube.Color.YELLOW);
             Logic.Location redBlue = LogicUtils.getLocationOfEdge(cube, Cube.Color.RED, Cube.Color.BLUE);
             Logic.Location redGreen = LogicUtils.getLocationOfEdge(cube, Cube.Color.RED, Cube.Color.GREEN);
@@ -53,13 +58,13 @@ public class LogicTest {
             assertNotNull(redBlue);
             assertNotNull(redGreen);
             assertNotNull(redWhite);
-            assertTrue(String.format(msgCross,"RED-YELLOW", redYellow.name, redYellow.secondDircetion,"UP - FRONT"),
+            assertTrue(msg1 + "RED-YELLOW" + msg2 + redYellow.name + " - " + redYellow.secondDircetion + msg3 + "UP - FRONT",
                     redYellow.name == Face_Enum.UP && redYellow.secondDircetion == Face_Enum.FRONT);
-            assertTrue(String.format(msgCross,"RED-BLUE", redBlue.name, redBlue.secondDircetion,"UP - RIGHT"),
+            assertTrue(msg1 + "RED-BLUE" + msg2 + redBlue.name + " - " + redBlue.secondDircetion + msg3 + "UP - RIGHT",
                     redBlue.name == Face_Enum.UP && redBlue.secondDircetion == Face_Enum.RIGHT);
-            assertTrue(String.format(msgCross,"RED-GREEN", redGreen.name, redGreen.secondDircetion,"UP - LEFT"),
+            assertTrue(msg1 + "RED-GREEN" + msg2 + redGreen.name + " - " + redGreen.secondDircetion + msg3 + "UP - LEFT",
                     redGreen.name == Face_Enum.UP && redGreen.secondDircetion == Face_Enum.LEFT);
-            assertTrue(String.format(msgCross,"RED-WHITE", redWhite.name, redWhite.secondDircetion,"UP - BACK"),
+            assertTrue(msg1 + "RED-WHITE" + msg2 + redWhite.name + " - " + redWhite.secondDircetion + msg3 + "UP - BACK",
                     redWhite.name == Face_Enum.UP && redWhite.secondDircetion == Face_Enum.BACK);
         }
     }
@@ -72,7 +77,8 @@ public class LogicTest {
             Logic.initialize(cube, actions);
             Logic.stageOne(cube, actions);
             Logic.stageTwo(cube, actions);
-            String msgCorner = "Stage Two has failed. %s  corner is not in place";
+            String msg1 = "Stage Two has failed. ";
+            String msg2 = "corner is not in place. ";
             Logic.Location redYellowBlue = LogicUtils.getLocationOfCorner(cube, Cube.Color.RED, Cube.Color.YELLOW, Cube.Color.BLUE);
             Logic.Location redGreenYellow = LogicUtils.getLocationOfCorner(cube, Cube.Color.RED, Cube.Color.GREEN, Cube.Color.YELLOW);
             Logic.Location redWhiteGreen = LogicUtils.getLocationOfCorner(cube, Cube.Color.RED, Cube.Color.WHITE, Cube.Color.GREEN);
@@ -81,13 +87,13 @@ public class LogicTest {
             assertNotNull(redGreenYellow);
             assertNotNull(redWhiteGreen);
             assertNotNull(redBlueWhite);
-            assertTrue( String.format(msgCorner,"RED-YELLOW-BLUE"), redYellowBlue.name == Face_Enum.UP
+            assertTrue(msg1+ "RED-YELLOW-BLUE" +msg2, redYellowBlue.name == Face_Enum.UP
                     && redYellowBlue.secondDircetion == Face_Enum.FRONT && redYellowBlue.thirdDirection == Face_Enum.RIGHT);
-            assertTrue(String.format(msgCorner,"RED-GREEN-YELLOW"),redGreenYellow.name == Face_Enum.UP
+            assertTrue(msg1+ "RED-GREEN-YELLOW" +msg2, redGreenYellow.name == Face_Enum.UP
                     && redGreenYellow.secondDircetion == Face_Enum.LEFT && redGreenYellow.thirdDirection == Face_Enum.FRONT);
-            assertTrue( String.format(msgCorner,"RED-WHITE-GREEN"),redWhiteGreen.name == Face_Enum.UP
+            assertTrue(msg1+ "RED-WHITE-GREEN" +msg2, redWhiteGreen.name == Face_Enum.UP
                     && redWhiteGreen.secondDircetion == Face_Enum.BACK && redWhiteGreen.thirdDirection == Face_Enum.LEFT);
-            assertTrue(String.format(msgCorner,"RED-BLUE-WHITE"),redBlueWhite.name == Face_Enum.UP
+            assertTrue(msg1+ "RED-BLUE-WHITE" +msg2, redBlueWhite.name == Face_Enum.UP
                     && redBlueWhite.secondDircetion == Face_Enum.RIGHT && redBlueWhite.thirdDirection == Face_Enum.BACK);
         }
     }
@@ -102,7 +108,10 @@ public class LogicTest {
             Logic.stageTwo(cube, actions);
             Logic.flipForStageThree(cube, actions);
             Logic.stageThree(cube, actions);
-            String msgEdge = "Stage Three test has failed. %s edge is located at : %s - %s  instead of : %s";
+            String msg1 = "Stage Three test has failed. ";
+            String msg2 = " edge is located at : ";
+            String msg3 = " - ";
+            String msg4 = "  instead of : ";
             Logic.Location whiteBlue = LogicUtils.getLocationOfEdge(cube, Cube.Color.WHITE, Cube.Color.BLUE);
             Logic.Location greenWhite = LogicUtils.getLocationOfEdge(cube, Cube.Color.GREEN, Cube.Color.WHITE);
             Logic.Location yellowGreen = LogicUtils.getLocationOfEdge(cube, Cube.Color.YELLOW, Cube.Color.GREEN);
@@ -111,13 +120,13 @@ public class LogicTest {
             assertNotNull(greenWhite);
             assertNotNull(yellowGreen);
             assertNotNull(blueYellow);
-            assertTrue(String.format(msgEdge,"WHITE-BLUE", whiteBlue.name, whiteBlue.secondDircetion,"FRONT - RIGHT"),
+            assertTrue(msg1+  "WHITE-BLUE" +msg2+ whiteBlue.name +msg3+ whiteBlue.secondDircetion +msg4+ "FRONT - RIGHT",
                     whiteBlue.name == Face_Enum.FRONT && whiteBlue.secondDircetion == Face_Enum.RIGHT);
-            assertTrue(String.format(msgEdge,"GREEN-WHITE", greenWhite.name, greenWhite.secondDircetion,"LEFT - FRONT"),
+            assertTrue(msg1+  "GREEN-WHITE" +msg2+ greenWhite.name +msg3+ greenWhite.secondDircetion +msg4+ "LEFT - FRONT",
                     greenWhite.name == Face_Enum.LEFT && greenWhite.secondDircetion == Face_Enum.FRONT);
-            assertTrue(String.format(msgEdge,"YELLOW-GREEN", yellowGreen.name, yellowGreen.secondDircetion,"BACK - LEFT"),
+            assertTrue(msg1+  "YELLOW-GREEN" +msg2+ yellowGreen.name +msg3+ yellowGreen.secondDircetion +msg4+ "BACK - LEFT",
                     yellowGreen.name == Face_Enum.BACK && yellowGreen.secondDircetion == Face_Enum.LEFT);
-            assertTrue(String.format(msgEdge,"BLUE-YELLOW", blueYellow.name, blueYellow.secondDircetion,"RIGHT - BACK"),
+            assertTrue(msg1+  "BLUE-YELLOW" +msg2+ blueYellow.name +msg3+ blueYellow.secondDircetion +msg4+ "RIGHT - BACK",
                     blueYellow.name == Face_Enum.RIGHT && blueYellow.secondDircetion == Face_Enum.BACK);
         }
     }
@@ -138,10 +147,10 @@ public class LogicTest {
             Logic.Location orangeGreen = LogicUtils.getLocationOfEdge(cube, Cube.Color.ORANGE, Cube.Color.GREEN);
             Logic.Location orangeYellow = LogicUtils.getLocationOfEdge(cube, Cube.Color.ORANGE, Cube.Color.YELLOW);
             Logic.Location[] oranges = {orangeBlue, orangeWhite, orangeGreen, orangeYellow};
-            String errMsg = "Stage four test has failed. this orange face should be UP, but instead it's %s";
+            String errMsg = "Stage four test has failed. this orange face should be UP, but instead it's ";
             for (int i = 0; i < 4; i++) {
                 assertNotNull(oranges[i]);
-                assertTrue( String.format(errMsg, oranges[i].name),oranges[i].name == Face_Enum.UP);
+                assertTrue(errMsg + oranges[i].name, oranges[i].name == Face_Enum.UP);
             }
         }
     }
@@ -159,10 +168,10 @@ public class LogicTest {
             Logic.stageFour(cube, actions);
             Logic.stageFive(cube, actions);
             Cube.Color[] oranges = {Cube.Color.WHITE, Cube.Color.GREEN, Cube.Color.YELLOW, Cube.Color.BLUE};
-            String errMsg = "Stage five test has failed. the color that caused the problem : %s";
-            for (int j = 0;j < 4; j++) {
+            String errMsg = "Stage five test has failed. the color that caused the problem : ";
+            for (int j = 0; j < 4; j++) {
                 assertNotNull(oranges[j]);
-                assertTrue(String.format(errMsg, cube.getFront().getGrid()[0][1]),
+                assertTrue(errMsg + cube.getFront().getGrid()[0][1],
                         cube.getFront().getGrid()[0][1] == oranges[j]);
                 cube.rotate(true);
             }
@@ -184,17 +193,18 @@ public class LogicTest {
             Logic.stageSix(cube, actions);
             Logic.Location corner1 = LogicUtils.getLocationOfCorner(cube, Cube.Color.ORANGE, Cube.Color.WHITE, Cube.Color.BLUE);
             Logic.Location corner2 = LogicUtils.getLocationOfCorner(cube, Cube.Color.ORANGE, Cube.Color.WHITE, Cube.Color.GREEN);
-            String errMsg = "Stage six test has failed. corner number %s is not in place";
+            String errMsg1 = "Stage six test has failed. corner number ";
+            String errMsg2 = "is not in place";
             assertNotNull(corner1);
             assertNotNull(corner2);
-            assertTrue(String.format(errMsg,"0"),
+            assertTrue(errMsg1 + "0" + errMsg2,
                     (corner1.name == Face_Enum.UP && corner1.secondDircetion == Face_Enum.FRONT && corner1.thirdDirection == Face_Enum.RIGHT) ||
                             (corner1.name == Face_Enum.RIGHT && corner1.secondDircetion == Face_Enum.UP && corner1.thirdDirection == Face_Enum.FRONT) ||
                             (corner1.name == Face_Enum.FRONT && corner1.secondDircetion == Face_Enum.RIGHT && corner1.thirdDirection == Face_Enum.UP) ||
                             (corner1.name == Face_Enum.UP && corner1.secondDircetion == Face_Enum.RIGHT && corner1.thirdDirection == Face_Enum.FRONT) ||
                             (corner1.name == Face_Enum.FRONT && corner1.secondDircetion == Face_Enum.UP && corner1.thirdDirection == Face_Enum.RIGHT) ||
                             (corner1.name == Face_Enum.RIGHT && corner1.secondDircetion == Face_Enum.FRONT && corner1.thirdDirection == Face_Enum.UP));
-            assertTrue( String.format(errMsg,"3"),
+            assertTrue(errMsg1 + "3"+ errMsg2,
                     (corner2.name == Face_Enum.UP && corner2.secondDircetion == Face_Enum.FRONT && corner2.thirdDirection == Face_Enum.LEFT) ||
                             (corner2.name == Face_Enum.LEFT && corner2.secondDircetion == Face_Enum.UP && corner2.thirdDirection == Face_Enum.FRONT) ||
                             (corner2.name == Face_Enum.FRONT && corner2.secondDircetion == Face_Enum.LEFT && corner2.thirdDirection == Face_Enum.UP) ||
@@ -207,19 +217,19 @@ public class LogicTest {
             Logic.Location corner4 = LogicUtils.getLocationOfCorner(cube, Cube.Color.ORANGE, Cube.Color.YELLOW, Cube.Color.BLUE);
             assertNotNull(corner3);
             assertNotNull(corner4);
-            assertTrue(String.format(errMsg,"2"),
+            assertTrue(errMsg1 + "2" + errMsg2,
                     (corner3.name == Face_Enum.UP && corner3.secondDircetion == Face_Enum.FRONT && corner3.thirdDirection == Face_Enum.RIGHT) ||
                             (corner3.name == Face_Enum.RIGHT && corner3.secondDircetion == Face_Enum.UP && corner3.thirdDirection == Face_Enum.FRONT) ||
                             (corner3.name == Face_Enum.FRONT && corner3.secondDircetion == Face_Enum.RIGHT && corner3.thirdDirection == Face_Enum.UP) ||
                             (corner3.name == Face_Enum.UP && corner3.secondDircetion == Face_Enum.RIGHT && corner3.thirdDirection == Face_Enum.FRONT) ||
                             (corner3.name == Face_Enum.FRONT && corner3.secondDircetion == Face_Enum.UP && corner3.thirdDirection == Face_Enum.RIGHT) ||
-                            (corner3.name == Face_Enum.RIGHT && corner3.secondDircetion == Face_Enum.FRONT && corner3.thirdDirection == Face_Enum.UP) );
-            assertTrue(String.format(errMsg,"1"),(corner4.name == Face_Enum.UP && corner4.secondDircetion == Face_Enum.FRONT && corner4.thirdDirection == Face_Enum.LEFT) ||
-                            (corner4.name == Face_Enum.LEFT && corner4.secondDircetion == Face_Enum.UP && corner4.thirdDirection == Face_Enum.FRONT) ||
-                            (corner4.name == Face_Enum.FRONT && corner4.secondDircetion == Face_Enum.LEFT && corner4.thirdDirection == Face_Enum.UP) ||
-                            (corner4.name == Face_Enum.UP && corner4.secondDircetion == Face_Enum.LEFT && corner4.thirdDirection == Face_Enum.FRONT) ||
-                            (corner4.name == Face_Enum.FRONT && corner4.secondDircetion == Face_Enum.UP && corner4.thirdDirection == Face_Enum.LEFT) ||
-                            (corner4.name == Face_Enum.LEFT && corner4.secondDircetion == Face_Enum.FRONT && corner4.thirdDirection == Face_Enum.UP));
+                            (corner3.name == Face_Enum.RIGHT && corner3.secondDircetion == Face_Enum.FRONT && corner3.thirdDirection == Face_Enum.UP));
+            assertTrue(errMsg1 + "1" + errMsg2, (corner4.name == Face_Enum.UP && corner4.secondDircetion == Face_Enum.FRONT && corner4.thirdDirection == Face_Enum.LEFT) ||
+                    (corner4.name == Face_Enum.LEFT && corner4.secondDircetion == Face_Enum.UP && corner4.thirdDirection == Face_Enum.FRONT) ||
+                    (corner4.name == Face_Enum.FRONT && corner4.secondDircetion == Face_Enum.LEFT && corner4.thirdDirection == Face_Enum.UP) ||
+                    (corner4.name == Face_Enum.UP && corner4.secondDircetion == Face_Enum.LEFT && corner4.thirdDirection == Face_Enum.FRONT) ||
+                    (corner4.name == Face_Enum.FRONT && corner4.secondDircetion == Face_Enum.UP && corner4.thirdDirection == Face_Enum.LEFT) ||
+                    (corner4.name == Face_Enum.LEFT && corner4.secondDircetion == Face_Enum.FRONT && corner4.thirdDirection == Face_Enum.UP));
         }
     }
 
@@ -238,7 +248,7 @@ public class LogicTest {
             Logic.stageSix(cube, actions);
             Logic.stageSeven(cube, actions);
             String errMsg = "Stage seven test has failed";
-            assertTrue( errMsg,Logic.isArnonReady(cube));
+            assertTrue(errMsg, Logic.isArnonReady(cube));
         }
     }
 
